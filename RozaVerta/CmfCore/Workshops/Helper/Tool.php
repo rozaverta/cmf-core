8ffd1bc924d45d77872954877275482ba4364792
goshav <gosha@rozaverta.com>
2019-04-30 00:32:26 +0300

    Update method Tool::compareProperties of the workshop helper

--- RozaVerta/CmfCore/Workshops/Helper/Tool.php
+++ RozaVerta/CmfCore/Workshops/Helper/Tool.php
@@ -10,6 +10,10 @@ namespace RozaVerta\CmfCore\Workshops\Helper;
 
 class Tool
 {
+	private function __construct()
+	{
+	}
+
 	/**
 	 * Compares values
 	 *
@@ -20,31 +24,40 @@ class Tool
 	 */
 	static public function compareProperties($a, $b, int $maxDepth = 512): bool
 	{
-		if( $maxDepth < 0 )
-		{
-			return false;
-		}
-
-		if( is_object($a))
+		if( is_object($a) )
 		{
 			$a = get_object_vars($a);
 		}
 
-		if( is_object($b) )
-		{
-			$b = get_object_vars($b);
-		}
-
 		if( is_array($a) )
 		{
-			if( !is_array($b) || count($a) !== count($b) )
+			if( is_object($b) )
+			{
+				$b = get_object_vars($b);
+			}
+
+			if( ! is_array($b) || count($a) !== count($b) )
 			{
 				return false;
 			}
 
 			foreach($a as $key => $value)
 			{
-				if( !array_key_exists($key, $b) || ! self::compareProperties($value, $b[$key], $maxDepth - 1) )
+				if( array_key_exists($key, $b) )
+				{
+					if( is_scalar($value) )
+					{
+						if( $value !== $b[$key] )
+						{
+							return false;
+						}
+					}
+					else if( $maxDepth < 1 || ! self::compareProperties($value, $b[$key], $maxDepth - 1) )
+					{
+						return false;
+					}
+				}
+				else
 				{
 					return false;
 				}
@@ -52,7 +65,13 @@ class Tool
 
 			return true;
 		}
-
-		return $a === $b;
+		else if( is_scalar($a) )
+		{
+			return $a === $b;
+		}
+		else
+		{
+			return false;
+		}
 	}
 }
